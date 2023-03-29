283c29131ee7adfb1f4d94542f9445c217965aea
wvss!m <benmechliawassim30@gmail.com>
2023-03-29 02:24:26 +0100

    setting up the postgreSQL data base

--- backend/main/settings.py
+++ backend/main/settings.py
@@ -12,9 +12,13 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 import os
 from pathlib import Path
+from dotenv import load_dotenv
+import dj_database_url
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+load_dotenv( BASE_DIR /'secret.env')
 
 
 # Quick-start development settings - unsuitable for production
@@ -81,10 +85,10 @@ WSGI_APPLICATION = 'main.wsgi.application'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-    'ENGINE': 'django.db.backends.sqlite3',
-    'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+    'default': dj_database_url.config(
+        default=os.environ.get("DEVELOPMENT_DB_URL"), conn_max_age=600
+    )
+    
 }
 
 # Password validation
