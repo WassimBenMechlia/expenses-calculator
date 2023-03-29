d165ffbf5adb677b9848420ef2e4ce160a71d6eb
wvss!m <benmechliawassim30@gmail.com>
2023-03-29 03:15:57 +0100

    static file config

--- backend/main/settings.py
+++ backend/main/settings.py
@@ -18,7 +18,7 @@ import dj_database_url
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-load_dotenv( BASE_DIR /'secret.env')
+load_dotenv( BASE_DIR / 'secret.env')
 
 
 # Quick-start development settings - unsuitable for production
@@ -126,8 +126,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
+STATIC_ROOT = os.path.join(BASE_DIR,'static')
 #STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
 #STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 # Default primary key field type
